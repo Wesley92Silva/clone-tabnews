2010fefa266099919db3f4e34e836e1f46fcc0ec
Wesley92Silva <58156855+Wesley92Silva@users.noreply.github.com>
2026-01-09 19:58:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Mensagem muito impactante</h1>
+  return <h1>Mensagem muito impactante</h1>;
+}
+
+function teste2() {
+  console.log("indentação errada!");
 }
 
 export default Home;
